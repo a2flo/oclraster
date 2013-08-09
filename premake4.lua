b7b9fe4055bfcf84664c327f5ffaf65348c8ddef
Florian Ziesche <florian.ziesche@gmail.com>
2013-08-09 18:42:09 +0200

    * updated tccpp submodule to fix some linux issues  * added "-Wno-nested-anon-types" to the clang build options to ignore this warning

--- premake4.lua
+++ premake4.lua
@@ -104,7 +104,7 @@ solution "oclraster"
 			buildoptions { "-Wno-unknown-warning-option" }
 			buildoptions { "-Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-header-hygiene -Wno-gnu -Wno-float-equal" }
 			buildoptions { "-Wno-documentation -Wno-system-headers -Wno-global-constructors -Wno-padded -Wno-packed" }
-			buildoptions { "-Wno-switch-enum -Wno-sign-conversion -Wno-conversion -Wno-exit-time-destructors" }
+			buildoptions { "-Wno-switch-enum -Wno-sign-conversion -Wno-conversion -Wno-exit-time-destructors -Wno-nested-anon-types" }
 			linkoptions { "-fvisibility=default" }
 			if(not win_unixenv) then
 				buildoptions { "-integrated-as" }
